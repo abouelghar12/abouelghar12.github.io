2196caa746cf92796adf39d1cb2d3b1bb3def641
abouelghar12 <142679087+abouelghar12@users.noreply.github.com>
2023-08-25 17:51:08 +0200

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,5 +1,5 @@
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("service-worker.js").then(registration => {
+    navigator.serviceWorker.register("https://abouelghar12.github.io/service-worker.js").then(registration => {
         console.log("SW Registered!");
     }).catch(error => {
         console.log("SW Registration Failed");
